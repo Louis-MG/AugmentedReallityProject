b8294ac8bf7a3ace4ec05f0c2a9e3c0849681bc3
lmgueguen <lm.gueguen@orange.fr>
2021-01-30 17:37:36 +0100

    Buttons to change languages are finished and functional

--- static/docu_functions.js
+++ static/docu_functions.js
@@ -1,12 +1,29 @@
 // functions for Documentation
 
 function change_to_english() {
-    document.getElementById("english").style.display = "block" ;
-    document.getElementById("french").style.display = "none" ;
+    var x = document.getElementById("english");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+      document.getElementById("french").style.display = "none";
+    } else {
+      x.style.display = "block";
+      document.getElementById("french").style.display = "none";
+    }
 }
 
 
 function change_to_french() {
-    document.getElementById("english").style.display = "none" ;
-    document.getElementById("french").style.display = "block" ;
+    var x = document.getElementById("french");
+    if (x.style.display === "none") {
+      x.style.display = "block" ;
+      document.getElementById("english").style.display = "none";
+    } else {
+      x.style.display = "block";
+      document.getElementById("english").style.display = "none";
+    }
 }
+
+
+
+// document.getElementById("english").style.display = "none" ;
+// document.getElementById("french").style.display = "block" ;
