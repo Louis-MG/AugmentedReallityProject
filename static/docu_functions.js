7077c14ffc4149555f547c456eb1c1274f7c438f
lmgueguen <lm.gueguen@orange.fr>
2021-01-30 15:51:55 +0100

    better visual and language switch button still WIP

---
+++ static/docu_functions.js
@@ -0,0 +1,12 @@
+// functions for Documentation
+
+function change_to_english() {
+    document.getElementById("english").style.display = "block" ;
+    document.getElementById("french").style.display = "none" ;
+}
+
+
+function change_to_french() {
+    document.getElementById("english").style.display = "none" ;
+    document.getElementById("french").style.display = "block" ;
+}
