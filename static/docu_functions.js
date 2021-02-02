536e381f1f7c0b5a74a2eff370263caab0864f3e
lmgueguen <lm.gueguen@orange.fr>
2021-02-02 10:25:50 +0100

    Added comments on the css, js and html code. All pictures and css ar now in assets fodler.

--- static/docu_functions.js
+++ static/docu_functions.js
@@ -1,18 +1,22 @@
 // functions for Documentation
 
+// These functions change the div displayed: there is one for each language. French is default. Last one if for the top left arrow button:
+
 function change_to_english() {
-    var x = document.getElementById("english");
+    var x = document.getElementById("english"); //looks for the english div
     if (x.style.display === "none") {
-      x.style.display = "block";
-      document.getElementById("french").style.display = "none";
+      x.style.display = "block"; //if not visible, changes the display to block
+      document.getElementById("french").style.display = "none"; //hides french and italian divs
       document.getElementById("italian").style.display = "none"
     } else {
-      x.style.display = "block";
-      document.getElementById("french").style.display = "none";
+      x.style.display = "block"; //if it was visible
+      document.getElementById("french").style.display = "none"; // hides french and italian
       document.getElementById("italian").style.display = "none"
     }
 }
 
+// other functions follow the exact same structure
+// when looking for an element, careful with getElementByID and getElementsById (stupid s)
 
 function change_to_french() {
     var x = document.getElementById("french");
@@ -42,9 +46,5 @@ function change_to_italian() {
 }
 
 function goHome() {
-    window.location.href = "home.html";
+    window.location.href = "home.html"; // sends the user back to the home menu
 }
-
-
-// document.getElementById("english").style.display = "none" ;
-// document.getElementById("french").style.display = "block" ;
