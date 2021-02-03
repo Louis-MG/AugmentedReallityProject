5c7570436221035a9b94465bd77f25f36526d55d
Tommaso <tommaso.barberis@etu.univ-lyon1.fr>
2021-02-03 14:00:30 +0100

    reaction update

--- static/index.js
+++ static/index.js
@@ -1,28 +1,4 @@
-import * as AR from "./aframe-ar.js";
+//import * as AR from "./aframe-ar.js";
 import {createReaction} from "./peptide-link.js";
 
-window.AR = AR;
-AR.registerComponent('connector', {
-    schema: {
-      src:  {type: 'selector'},
-      dest: {type: 'selector'},
-      alpha: {type: 'number', default: 0.5}
-    },
-  
-    init: function() {
-      this.p0 = new THREE.Vector3();
-      this.p1 = new THREE.Vector3();
-      this.pf = new THREE.Vector3();
-    },
-  
-    tick: function() {
-      var obj0 = this.data.src.object3D;
-      var obj1 = this.data.dest.object3D;
-      this.p0.setFromMatrixPosition(obj0.matrixWorld);
-      this.p1.setFromMatrixPosition(obj1.matrixWorld);
-      this.pf.lerpVectors(this.p0, this.p1, this.data.alpha);
-      this.el.setAttribute("position", this.pf);
-    },
-  });
-
-  setInterval(createReaction(), 200);
+setInterval(createReaction(), 200);
--- static/peptide-link.js
+++ static/peptide-link.js
@@ -1,41 +1,45 @@
 function createReaction() {
-    console.log("loading new reaction...");
-    var pep=0;
-
-    const scene = document.getElementById("thescene")
-    scene.object3D.updateMatrixWorld(); //select the scene
-    var p1 = new THREE.Vector3(); p1.setFromMatrixPosition(document.getElementById("aminoAcid1").object3D.matrixWorld); //set the object aminoacid1 in the scene
-    var p2 = new THREE.Vector3(); p1.setFromMatrixPosition(document.getElementById("aminoAcid2").object3D.matrixWorld); //set the object aminoacid2 in the scene    
-    let distAa1Aa2 = 2 * Math.sqrt( Math.pow(p1.x - p2.x, 2) + Math.pow(p1.y - p2.y, 2)); //distance between the two molecules
-    const divInfop = document.getElementById("infop")
-    divInfop.innerHTML = "<p>" + distAa1Aa2 + "</p>"; //shows distance
-
-    // if the two molecules are adequately close, formation of the dipeptide
+    //console.log("loading new reaction...");
 
     const aminoAcid1 = document.getElementById("aminoAcid1");
     const aminoAcid2 = document.getElementById("aminoAcid2");
-    //const dipeptide = document.getElementById("dipeptide");
-    //const water = document.getElementById("water");
+    const dipeptide = document.getElementById("dipeptide");
+    const water = document.getElementById("water");
+    const divInfop = document.getElementById("infop")
+    const aa2Text = document.getElementById("aminoAcid2Text")
+    const wat2Text = document.getElementById("waterText")
 
-    if (distAa1Aa2 < 2 && distAa1Aa2 < 1){
-        aminoAcid1.setAttribute("visible", false) // marker hiro
-        aminoAcid2.setAttribute("visible", false) // marker kanji
-        //dipeptide.setAttribute("visible", true)  // marker hiro
-        //water.setAttribute("visible", true) // marker kanji
-        pep++
-    }
-    else if (distAa1Aa2 > 2 && distAa1Aa2 < 1){ //if the distance between the molecules (markers) increase more then 2 amstrong
-        aminoAcid1.setAttribute("visible", true) // marker hiro
-        aminoAcid2.setAttribute("visible", true) // marker kanji
-        //dipeptide.setAttribute("visible", false) // marker hiro
-        //water.setAttribute("visible", false) // marker kanji
-    }
 
-    // if the marker of aminoacid2 disappears after the reaction
+    document.getElementById("thescene").object3D.updateMatrixWorld(); //select the scene
+    var p1 = new THREE.Vector3(); p1.setFromMatrixPosition(aminoAcid1.object3D.matrixWorld); //set the object aminoacid1 in the scene
+    var p2 = new THREE.Vector3(); p2.setFromMatrixPosition(aminoAcid2.object3D.matrixWorld); //set the object aminoacid2 in the scene    
+    let distAa1Aa2 = 2 * Math.sqrt( Math.pow(p1.x - p2.x, 2) + Math.pow(p1.y - p2.y, 2) + Math.pow(p1.z-p2.z,2)); //distance between the two molecules
+    divInfop.innerHTML = "<p>" + distAa1Aa2 + "</p>"; //shows distance
 
-    if (pep == 1 && document.querySelector("#kanjiMarkerSelector").object3D.visible == false){
-        aminoAcid2.setAttribute('visible', true)
+    if (distAa1Aa2 > 2.5){
+        if (document.querySelector("#letterAMarkerSelector").object3D.visible == true){
+            aminoAcid1.setAttribute('visible',true)
+            water.setAttribute('visible',false)
+            wat2Text.setAttribute('visible',false)
+        }
+        if (document.querySelector("#kanjiMarkerSelector").object3D.visible == true){
+            aminoAcid2.setAttribute('visible',true)
+            aa2Text.setAttribute('visible',true)
+            dipeptide.setAttribute('visible',false)
+        }
+    }else if (distAa1Aa2 < 2.5){
+        if (document.querySelector("#letterAMarkerSelector").object3D.visible == true){
+            aminoAcid1.setAttribute('visible',false)
+            water.setAttribute('visible',true)
+            wat2Text.setAttribute('visible',true)
+        }
+        if (document.querySelector("#kanjiMarkerSelector").object3D.visible == true){
+            aminoAcid2.setAttribute('visible',false)
+            aa2Text.setAttribute('visible',false)
+            dipeptide.setAttribute('visible',true)
+        }
     }
 };
 
-export {createReaction};
+//export {createReaction};
+setInterval(createReaction, 200);
