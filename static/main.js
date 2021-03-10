92707ec510f31fd05e541a07062a1d824e778ee5
Nath <racoonatha@icloud.com>
2021-03-10 11:48:33 +0100

    materialize responsive grids & icons integration

--- static/main.js
+++ static/main.js
@@ -7,7 +7,7 @@ import * as reac_4 from "./module/esterification.js";
 
 //const footer = document.getElementsByClassName('footer')[0];
 const moleculeBackground = document.getElementById("bgvid");
-const container1 = document.getElementsByClassName('container1')[0];
+const container1 = document.getElementById('container1');
 const bigTitle = document.getElementsByClassName("bigTitle")[0];
 const reaction = document.getElementById('reaction');
 const documentation_ar = document.getElementById('docu_ar');
@@ -21,57 +21,28 @@ const italian = document.getElementById("italian");
 const italian1 = document.getElementById("italian1");
 
 function collection() {
-    let i = 1; // reactions counter
-    let height = 600; // reactions container height in pixel
-    let top = 0; // box y position in pixel
-    let previousPosition = 0; // 0:Start, 1:66%R, 2:Mid, 3:66%L  position de la box precedente
     let scaffold = '';
-    let positionAttr = 'right: 66%';
-    let transformAttr = ';';
-    do{ // reaction div crafting
+    let i = 1; // reactions counter
+    do { // reaction div crafting
         scaffold += `
-        <div id="box${i}" style="${positionAttr};top:${top}px;position: fixed;background-color: white;box-shadow: rgba(0, 0, 0, 0.3) 0 0 20px;border-collapse: collapse;border-radius: 20px;width: 350px;height: 230px${transformAttr}">
-            <div class="mediumTitle">${eval('reac_'+String(i)+".data['name'];")}</div>
-            <div class="descriptionText">${eval('reac_'+String(i)+".data['description'];")}</div>
-            <button type="button" class="buttonD" id="button${i}">
-                <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" fill="currentColor" class="bi bi-arrow-right-short" viewBox="1 0 16 16">
-                    <path fill-rule="evenodd" d="M4 8a.5.5 0 0 1 .5-.5h5.793L8.146 5.354a.5.5 0 1 1 .708-.708l3 3a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708-.708L10.293 8.5H4.5A.5.5 0 0 1 4 8z"/>
-                </svg>
-            </button>
-            ${eval('reac_'+String(i)+".data['image'];")}
+        <div class="col s12 m4 l3 xl2" style="padding-top: 40px">
+            <div id="box${i}" style="position: relative;left: 50%;transform: translate(-50%, 0);background-color: white;box-shadow: rgba(0, 0, 0, 0.3) 0 0 20px;border-collapse: collapse;border-radius: 20px;width: 350px;height: 230px">
+                <div class="mediumTitle">${eval('reac_' + String(i) + ".data['name'];")}</div>
+                <div class="descriptionText">${eval('reac_' + String(i) + ".data['description'];")}</div>
+                <button type="button" class="buttonD" id="button${i}">
+                    <i class="small material-icons">arrow_forward</i>
+                </button>
+                ${eval('reac_' + String(i) + ".data['image'];")}
+            </div>
         </div>
         `;
         scaffold + '\n';
-        if(previousPosition===3){ // next floor
-            previousPosition = 1;
-            positionAttr = 'right: 66%';
-            transformAttr = ';';
-            height += 300;
-            top += 263;
-            container1.style.height = `${height}px`;
-        }else{ // next box position
-            if(previousPosition===0){previousPosition=2}else{previousPosition++}
-            if(previousPosition===2){
-                positionAttr = 'left: 50%';
-                transformAttr = ';transform: translate(-50%, 0);';
-            }else{
-                positionAttr = 'left: 66%';
-                transformAttr = ';';
-            }
-        }
-        i++; // next import...
+        i++;
     }
     while (eval('typeof reac_'+String(i)+';') !== 'undefined');
 
     container1.innerHTML = scaffold;
     console.log(scaffold);
-
-    for(var j = 1; j<i; j++){ //click event for each button
-        console.log(j);
-        eval('const button'+j+"= document.getElementById('button"+j+"')");
-        eval('button'+j+".addEventListener('click', loadReaction, false);");
-        eval('button'+j+'.param='+j+';')
-    }
 }
 
 collection();
