755f6ef00e6ba39ece574193d385ae7e685c8480
Nath <racoonatha@icloud.com>
2021-03-12 18:56:15 +0100

    materialize footer + 2D reac preview

--- static/main.js
+++ static/main.js
@@ -5,6 +5,7 @@ const moleculeBackground = document.getElementById("bgvid");
 const container1 = document.getElementById('container1');
 const bigTitle = document.getElementsByClassName("bigTitle")[0];
 const divReaction = document.getElementById('reaction');
+const footer = document.querySelector('footer');
 const documentation_ar = document.getElementById('docu_ar');
 const documentation_reac = document.getElementById('docu_reac');
 const languageButtons = document.getElementsByClassName('button_box')[0];
@@ -121,12 +122,16 @@ function showReactionPage(){
     //footer.style.display = 'block';
     bigTitle.innerHTML = "Choose a reaction";
     moleculeBackground.style.display = 'block';
+    footer.style.marginTop = '300px';
+    documentation_ar.style.display = "none"
+    documentation_reac.style.display = "none"
     camera(false);
     reaction.innerHTML = `
     <div id="child"></div>
     `;
-    //if (document.getElementById("arjs-video") !== null){ // to force kill the video flux
-    //location.reload(); // only way I found is too reload the page
+    if (document.getElementById("arjs-video") !== null) { // to force kill the video flux
+        location.reload(); // only way I found is too reload the page
+    }
 }
 
 buttonA.addEventListener('click', showReactionPage);
@@ -135,6 +140,7 @@ function loadReaction(evt){
     let nbReaction = evt.currentTarget.param;
     let moduleReaction = moduleArray[nbReaction]
     console.log('loading '+moduleReaction.data['title']+' reaction...');
+    footer.style.display = 'none'
     hideReactionPage();
     bigTitle.innerHTML = moduleReaction.data['name'];
     camera(true);
@@ -149,128 +155,6 @@ function load (divRoot, nbReaction){
     window.currentReaction = Reaction(nbReaction);
 }
 
-// functions for Documentation
-
-// These functions change the div displayed: there is one for each language. French is default. Last one if for the top left arrow button:
-
-function change_to_english() {
-    // changes the language div displayed in function of the documentation the user is in
-    french.style.display = "none";
-    french1.style.display = "none";
-    italian.style.display = "none";
-    italian1.style.display = "none";
-    if (documentation_ar.style.display === "block") {
-        english1.style.display = "none";
-        english.style.display = "block";
-    } else {
-        english.style.display = "none";
-        english1.style.display = "block";
-    }
-}
-
-function change_to_french() {
-    // changes the language div displayed in function of the documentation the user is in
-    english.style.display = "none";
-    english1.style.display = "none";
-    italian.style.display = "none";
-    italian1.style.display = "none";
-    if (documentation_ar.style.display === "block") {
-        french1.style.display = "none";
-        french.style.display = "block";
-    } else {
-        french.style.display = "none";
-        french1.style.display = "block";
-    }
-}
-
-function change_to_italian() {
-    // changes the language div displayed in function of the documentation the user is in
-    french.style.display = "none";
-    french1.style.display = "none";
-    english.style.display = "none";
-    english1.style.display = "none";
-    if (documentation_ar.style.display === "block") {
-        italian1.style.display = "none";
-        italian.style.display = "block";
-    } else {
-        italian.style.display = "none";
-        italian1.style.display = "block";
-    }
-}
-
-const button_french = document.getElementById("language1")
-button_french.addEventListener('click', change_to_french)
-
-const button_english = document.getElementById("language2")
-button_english.addEventListener('click', change_to_english)
-
-const button_italian = document.getElementById("language3")
-button_italian.addEventListener('click', change_to_italian)
-
-// gotta change that for a display change:
-
-// function goHome() {
-//     window.location.href = "home.html"; // sends the user back to the home menu
-// }
-
-const buttonB = document.getElementById('buttonB');
-
-function goDocu_ar() {
-    var x = documentation_ar;
-    hideReactionPage();
-    reaction.style.display = "none";  //hides reaction page
-    bigTitle.style.display = 'none';
-    killRunningReaction();
-    documentation_reac.style.display = "none"; //hides the other documentation
-    languageButtons.style.display = "block";
-    if (x.style.display === "none") {
-        x.style.display = "block";
-        if (english1.style.display === "block") {  // conditional display of languages
-            english1.style.display = "none"
-            english.style.display = "block";
-        } else if (french1.style.display === "block") {
-            french1.style.display = "none";
-            french.style.display = "block";
-        } else if (italian1.style.display === "block") {
-            italian1.style.display = "none";
-            italian.style.display = "block";
-        }
-    } else {
-        x.style.display = "block";
-    }
-}
-
-buttonB.addEventListener('click', goDocu_ar);
-
-function goDocu_reac() {
-    //see goDocu_ar for comments
-    var x = documentation_reac;
-    hideReactionPage();
-    reaction.style.display = "none";
-    killRunningReaction();
-    bigTitle.style.display = 'none';
-    documentation_ar.style.display = "none";
-    languageButtons.style.display = "block";
-    if (x.style.display === "none") {
-        x.style.display = "block";
-        if (english.style.display === "block") {
-            english.style.display = "none";
-            english1.style.display = "block";
-        } else if (french.style.display === "block") {
-            french.style.display = "none";
-            french1.style.display = "block";
-        } else if (italian.style.display === "block") {
-            italian.style.display = "none";
-            italian1.style.display = "block";
-        }
-    } else {
-        x.style.display = "block";
-    }
-}
-
-const buttonC = document.getElementById("buttonC");
-buttonC.addEventListener('click', goDocu_reac)
-
 // create the a-frame scene
 function createScaffold(divRoot, nbReaction) {
     let moduleReaction = moduleArray[nbReaction];
@@ -278,6 +162,12 @@ function createScaffold(divRoot, nbReaction) {
     <!-- reaction information -->
     <div class="mainText">
         <p id="info"><b>${moduleReaction.data['description']}</b></p>
+        <div id="infoImage">${moduleReaction.data['infoImage']}</div>
+        <div id="b2" style="display: none">
+            <button type="button" id="buttonE">
+                <i class="small material-icons">arrow_forward</i>
+            </button>
+        </div>
     </div>
 
     <!-- a-frame scene reaction -->
@@ -285,6 +175,14 @@ function createScaffold(divRoot, nbReaction) {
 
     </a-scene>
   `
+    console.log(moduleReaction.data['infoImage'])
+    if(typeof moduleReaction.data['infoImage']!=='undefined'){ // if an 2d reac image is provided
+        const buttonE = document.getElementById("buttonE"); // click to see 2D reaction infos
+        buttonE.addEventListener('click', display2Dreaction)
+        document.getElementById('b2').style.display = 'block'
+    }
+    const info = document.getElementById('info');
+    info.style.display = 'block'; // bug workaround for button E
 
     let aFrameScene = document.getElementById('theScene');
     let table = moduleReaction.data["type"];
@@ -308,6 +206,20 @@ function createScaffold(divRoot, nbReaction) {
         scaffoldType1(aFrameScene, table);
     }else if (sizeReagents === 3 && sizeProducts === 1 && sizeConditions === 0) { //ex. water formation
         scaffoldType2(aFrameScene, table);
+    }
+}
+
+function display2Dreaction(){
+    console.log('loading 2D reaction informations...');
+    const info = document.getElementById('info');
+    const infoImage = document.getElementById('infoImage');
+    console.log(info.style.display)
+    if(info.style.display !== 'block'){
+        infoImage.style.display = 'none'; // hide 2D reaction image
+        info.style.display = 'block'; // show the text
+    }else{
+        infoImage.style.display = 'block'; // show 2D reaction image
+        info.style.display = 'none'; // hide the text
     }
 }
 
@@ -434,3 +346,127 @@ function scaffoldType2 (aFrameScene, table) { // for H20 molecule...
         }
     }
 }
+
+// functions for Documentation
+
+// These functions change the div displayed: there is one for each language. French is default. Last one if for the top left arrow button:
+
+function change_to_english() {
+    // changes the language div displayed in function of the documentation the user is in
+    french.style.display = "none";
+    french1.style.display = "none";
+    italian.style.display = "none";
+    italian1.style.display = "none";
+    if (documentation_ar.style.display === "block") {
+        english1.style.display = "none";
+        english.style.display = "block";
+    } else {
+        english.style.display = "none";
+        english1.style.display = "block";
+    }
+}
+
+function change_to_french() {
+    // changes the language div displayed in function of the documentation the user is in
+    english.style.display = "none";
+    english1.style.display = "none";
+    italian.style.display = "none";
+    italian1.style.display = "none";
+    if (documentation_ar.style.display === "block") {
+        french1.style.display = "none";
+        french.style.display = "block";
+    } else {
+        french.style.display = "none";
+        french1.style.display = "block";
+    }
+}
+
+function change_to_italian() {
+    // changes the language div displayed in function of the documentation the user is in
+    french.style.display = "none";
+    french1.style.display = "none";
+    english.style.display = "none";
+    english1.style.display = "none";
+    if (documentation_ar.style.display === "block") {
+        italian1.style.display = "none";
+        italian.style.display = "block";
+    } else {
+        italian.style.display = "none";
+        italian1.style.display = "block";
+    }
+}
+
+const button_french = document.getElementById("language1")
+button_french.addEventListener('click', change_to_french)
+
+const button_english = document.getElementById("language2")
+button_english.addEventListener('click', change_to_english)
+
+const button_italian = document.getElementById("language3")
+button_italian.addEventListener('click', change_to_italian)
+
+// gotta change that for a display change:
+
+// function goHome() {
+//     window.location.href = "home.html"; // sends the user back to the home menu
+// }
+
+const buttonB = document.getElementById('buttonB');
+
+function goDocu_ar() {
+    var x = documentation_ar;
+    hideReactionPage();
+    reaction.style.display = "none";  //hides reaction page
+    bigTitle.style.display = 'none';
+    killRunningReaction();
+    documentation_reac.style.display = "none"; //hides the other documentation
+    languageButtons.style.display = "block";
+    footer.style.marginTop = '0';
+    if (x.style.display === "none") {
+        x.style.display = "block";
+        if (english1.style.display === "block") {  // conditional display of languages
+            english1.style.display = "none"
+            english.style.display = "block";
+        } else if (french1.style.display === "block") {
+            french1.style.display = "none";
+            french.style.display = "block";
+        } else if (italian1.style.display === "block") {
+            italian1.style.display = "none";
+            italian.style.display = "block";
+        }
+    } else {
+        x.style.display = "block";
+    }
+}
+
+buttonB.addEventListener('click', goDocu_ar);
+
+function goDocu_reac() {
+    //see goDocu_ar for comments
+    var x = documentation_reac;
+    hideReactionPage();
+    reaction.style.display = "none";
+    killRunningReaction();
+    bigTitle.style.display = 'none';
+    documentation_ar.style.display = "none";
+    languageButtons.style.display = "block";
+    footer.style.marginTop = '0';
+    if (x.style.display === "none") {
+        x.style.display = "block";
+        if (english.style.display === "block") {
+            english.style.display = "none";
+            english1.style.display = "block";
+        } else if (french.style.display === "block") {
+            french.style.display = "none";
+            french1.style.display = "block";
+        } else if (italian.style.display === "block") {
+            italian.style.display = "none";
+            italian1.style.display = "block";
+        }
+    } else {
+        x.style.display = "block";
+    }
+}
+
+const buttonC = document.getElementById("buttonC");
+buttonC.addEventListener('click', goDocu_reac)
