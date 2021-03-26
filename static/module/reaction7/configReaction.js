d0a6897fb11827d34153d3d36bb1a919356265ff
Louis-Mael GUEGUEN <lm.gueguen@orange.fr>
2021-03-26 17:28:01 +0100

    reactions 6 and 7 WIP

--- static/module/reaction7/configReaction.js
+++ static/module/reaction7/configReaction.js
@@ -1,9 +1,9 @@
 export const data = {
-    name: "Oxydation of aldoses to aldonic acid",
-    title: "Aldonic acid formation",
+    name: "Reduction of aldoses to alditols",
+    title: "Alditol formation",
     description: "Alditols are organic compounds, typically derived from sugars, containing one hydroxyle group attached to each carbon atom.",
-    image: "<img src='/static/module/reaction6/icon.png' style='width: 100%;position: relative; top:80px; left:-80px'>",
-    infoImage: "<img src='/static/module/reaction6/infoImage.png'>",//max weight : 500px
+    image: "<img src='/static/module/reaction5/icon.png' style='width: 100%;position: relative; top:80px; left:-80px'>",
+    infoImage: "<img src='/static/module/reaction5/infoImage.png'>",//max weight : 500px
     
     /*
     For the reagents, products and experimental condition,
@@ -14,13 +14,13 @@ export const data = {
         // list of reagents
         reagents: {
             reagent1: ["Dgluc.obj", "Dgluc.mtl", "letterA"],
-            reagent2: ["NitricAcid.obj", "NitricAcid.mtl", "kanji"],
+            reagent2: ["Methanol.obj", "Methanol.mtl", "kanji"],
         },
 
         // list of products
         products: {
-            product1: ["DglucAcid.obj", "DglucAcid.mtl", "letterA"],
-            product2: ["NitricAcid.obj", "NitricAcid.mtl", "kanji"]
+            product1: [".obj", ".mtl", "letterA"],
+            product2: [".obj", ".mtl", "kanji"]
         },
     },
     // list of condition
