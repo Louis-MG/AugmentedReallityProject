75ee4d45c385b821b581b498acc41c0c361682a0
Louis-Mael GUEGUEN <lm.gueguen@orange.fr>
2021-03-26 17:19:10 +0100

    added reac 6 and 7 WIP

---
+++ static/module/reaction6/configReaction.js
@@ -0,0 +1,29 @@
+export const data = {
+    name: "Reduction of aldoses to alditols",
+    title: "Alditol formation",
+    description: "Alditols are organic compounds, typically derived from sugars, containing one hydroxyle group attached to each carbon atom.",
+    image: "<img src='/static/module/reaction5/icon.png' style='width: 100%;position: relative; top:80px; left:-80px'>",
+    infoImage: "<img src='/static/module/reaction5/infoImage.png'>",//max weight : 500px
+    
+    /*
+    For the reagents, products and experimental condition,
+    please indicate a .obj file, a .mtl file and a preset marker.
+    */
+   
+    type: {
+        // list of reagents
+        reagents: {
+            reagent1: ["Glyceraldheyde.obj", "Glyceraldheyde.mtl", "letterA"],
+            reagent2: ["NaBH4.obj", "NaBH4.mtl", "kanji"],
+        },
+
+        // list of products
+        products: {
+            product1: ["Glyceritol.obj", "Glyceritol.mtl", "letterA"],
+            product2: ["NaBH4.obj", "NaBH4.mtl", "kanji"]
+        },
+    },
+    // list of condition
+    conditions: {
+    },
+};
